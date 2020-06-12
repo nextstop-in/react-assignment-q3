4bd93100d24e05104324ceedc35f70afd8afe6e8
nextstop-in <shanu801304@gmail.com>
2020-06-12 15:51:33 +0530

    modified App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Switch, Route } from "react-router-dom";
 import Homepage from "./component/Homepage";
 import SignIn from "./component/SignIn";
 import ShopPage from "./component/ShopPage";
-import UserData from "./component/userdata";
+import UserData from "./component/UserData";
 
 function App() {
   return (
